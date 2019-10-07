23d14a4f41214a0c7ec469cec223135c12dd1054
David Thompson <davethompson21@gmail.com>
2019-10-07 16:42:15 +0100

    Non-breaking entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,17 +2,17 @@
 set -e
 
 echo "--> Checking required environment variables"
-(
-    [ -z "$CLUSTER_NAME" ] ||
-    [ -z "$AWS_REGION" ] ||
-    [ -z "$AWS_ACCESS_KEY_ID" ] ||
-    [ -z "$AWS_SECRET_ACCESS_KEY" ]
-) && echo "ERROR: You must set env vars: CLUSTER_NAME, AWS_REGION, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID" && exit 1
-echo "✅ environment variables present"
-echo "--> Updating kubeconfig for access to \"${CLUSTER_NAME}\" in region \"${AWS_REGION}\""
-aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${AWS_REGION}
-echo "--> Verifying kubectl access"
-kubectl version
-[ $? == 0 ] && echo "✅ kubectl access verified"
+if [ -n "$CLUSTER_NAME" -a -n "$AWS_REGION" -a -n "$AWS_ACCESS_KEY_ID" -a -n "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "✅ environment variables present"
+  echo "--> Updating kubeconfig for access to \"${CLUSTER_NAME}\" in region \"${AWS_REGION}\""
+  aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${AWS_REGION}
+  echo "--> Verifying kubectl access"
+  kubectl version
+  [ $? == 0 ] && echo "✅ kubectl access verified"
+else
+  echo "⚠️  kubectl context not configured:"
+  echo "Please set env vars CLUSTER_NAME, AWS_REGION, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and run:"
+  echo "aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${AWS_REGION}"
+fi
 
 exec "$@"
